33a5c0e6a1e70ab8aa7e045b91727b8dcfeb8a23
Shawn <hjwshawn96@gmail.com>
2025-04-28 02:39:59 +0800

    fixed indentation error

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ websites = [
 
 # Website status dictionary
 status = {}
-def check_websites():
+  def check_websites():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
     }
